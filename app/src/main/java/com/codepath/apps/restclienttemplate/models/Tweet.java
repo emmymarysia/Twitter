2eb9da6a2ba50cf236d86bdff695c368dec34b9d
Emily Thompson <emilymthompson@fb.com>
2022-06-08 17:21:00 -0700

    display images in tweets

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -1,5 +1,7 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import android.util.Log;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -14,19 +16,37 @@ public class Tweet {
     public String body;
     public String createdAt;
     public User user;
+    public String mediaUrl;
+    public boolean hasMedia;
 
-    //empty constructer needed by the Parceler library
+    //empty constructor needed by the Parceler library
     public Tweet() {
 
     }
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
-        tweet.body = jsonObject.getString("text");
+        if(jsonObject.has("full_text")) {
+            tweet.body = jsonObject.getString("full_text");
+        }
+        else {
+            tweet.body = jsonObject.getString("text");
+        }
         tweet.createdAt = jsonObject.getString("created_at");
         tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        JSONArray media = jsonObject.getJSONObject("entities").optJSONArray("media");
+        if(media != null) {
+            tweet.mediaUrl = media.getJSONObject(0).getString("media_url_https");
+            tweet.hasMedia = true;
+        }
+        else {
+            tweet.mediaUrl = "";
+            tweet.hasMedia = false;
+        }
         return tweet;
     }
+
+    //public String getRelativeTimeAgo
 
     public static List<Tweet> fromJsonArray(JSONArray jsonArray) throws JSONException {
         List<Tweet> tweets = new ArrayList<>();
